3db192548487b257c892ec489b101ee1d5fe692d
sahil <sahilmulani501@gmail.com>
2024-03-01 18:05:05 +0530

    rollback to starter code

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-from semantics import generate_embedding,model,index
+from semantics import generate_embedding,index
 app = FastAPI()
-
-class RequestData(BaseModel):
-    client_id: str
-    data: str
-    key:int
-class QueryData(BaseModel):
-    query: str
 
 @app.get('/')
 async def home_page():
@@ -18,25 +11,19 @@ async def home_page():
     }
 
 @app.post("/generate_embeddings/")
-async def generate_embeddings(request_data: RequestData):
-    try:
-        client_id = request_data.client_id
-        json_data = {request_data.key: request_data.data}
-        embeddings = generate_embedding(json_data,client_id)
-        return {"message": "Database created successfully"}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+async def generate_embeddings():
+    print("got embedding request")
+    generate_embedding()
+    return {
+        "status": True,
+        "message":"generating embeddings..."
+    }
 
 
 @app.post("/query_data/")
-async def query_data(query_data: QueryData):
-    try:
-        encode=model.encode(query_data.query)
-        answer=index.query(vector=encode, top_k=2, include_metadata=True, include_vectors=False)
-        for vec in answer:
-            question = vec.metadata["Data"]
-            # print(f"Answer: {question}")
-            return {"message": "Query data processed successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+async def query_data():
+    print("got query data request")
+    return {
+        "status": True,
+        "message":"Query data request..."
+    }
--- backend/semantics.py
+++ backend/semantics.py
@@ -1,32 +1,13 @@
-from sentence_transformers import SentenceTransformer
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import json
 from dotenv import load_dotenv
 import os
 from upstash_vector import Index
 
-MODEL = "multi-qa-MiniLM-L6-cos-v1"
-model = SentenceTransformer(MODEL, device="cpu")
 load_dotenv()
 Url = os.getenv("URL")
 Token = os.getenv("Token")
 index = Index(url=Url, token=Token)
 
-def generate_embedding(json_data: str, client_id: str) -> None:
-    data = json.dumps(json_data)
-    spilter = RecursiveCharacterTextSplitter(
-        separators=['\n\n', '\n', '\n\n\n', '.', '\t'],
-        chunk_size=500,
-        chunk_overlap=0
-    )
-    chunks = spilter.split_text(data)
-    
-    for id, value in enumerate(chunks):
-        # Split each chunk into smaller parts before encoding
-        sub_chunks = [value[i:i+100] for i in range(0, len(value), 100)]
-        for sub_chunk in sub_chunks:
-            result_vectors = model.encode([sub_chunk]).tolist()
-            vector_with_metadata = {"id": str(id), "vector": result_vectors[0], "metadata": {"client_id": client_id, "Data": sub_chunk}}
-            index.upsert(vectors=[vector_with_metadata])
-
-    print(f"Generated {len(chunks)} embeddings.")
+def generate_embedding():
+  print("generating embedding...")
+  
+ 
