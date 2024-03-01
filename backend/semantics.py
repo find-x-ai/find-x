368972a7d0f2ddc86ee79d6ff8a75ebf7190df5e
sahil <sahilmulani501@gmail.com>
2024-03-01 18:33:36 +0530

    fix small issue regarding import

--- backend/semantics.py
+++ backend/semantics.py
@@ -1,18 +1,13 @@
-from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 import os
 from upstash_vector import Index
 
-
-
+load_dotenv()
+Url = os.getenv("URL")
+Token = os.getenv("Token")
+index = Index(url=Url, token=Token)
 
 def generate_embedding():
-  MODEL = "multi-qa-MiniLM-L6-cos-v1"
-  model = SentenceTransformer(MODEL)
-  load_dotenv()
-  Url = os.getenv("URL")
-  Token = os.getenv("Token")
-  index = Index(url=Url, token=Token)
   print("Generating embedding...")
   
  
