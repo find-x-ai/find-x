7c22e658260b97e1615c9522eac57183d482ba73
sahil <sahilmulani501@gmail.com>
2024-03-01 15:11:16 +0530

    data error fix

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-import json
 from semantics import generate_embedding,model,index
 app = FastAPI()
 
@@ -26,5 +25,18 @@ async def generate_embeddings(request_data: RequestData):
         embeddings = generate_embedding(json_data,client_id)
         return {"message": "Database created successfully"}
     
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/query_data/")
+async def query_data(query_data: QueryData):
+    try:
+        encode=model.encode(query_data.query)
+        answer=index.query(vector=encode, top_k=2, include_metadata=True, include_vectors=False)
+        for vec in answer:
+            question = vec.metadata["Data"]
+            # print(f"Answer: {question}")
+            return {"message": "Query data processed successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/semantics.py
+++ backend/semantics.py
@@ -4,24 +4,26 @@ import json
 from dotenv import load_dotenv
 import os
 from upstash_vector import Index
+
 MODEL = "multi-qa-MiniLM-L6-cos-v1"
 model = SentenceTransformer(MODEL, device="cpu")
 load_dotenv()
-Url=os.getenv("URL")
-Token=os.getenv("Token")
+Url = os.getenv("URL")
+Token = os.getenv("Token")
 index = Index(url=Url, token=Token)
-def generate_embedding(json_data: str,client_id: str) -> list[list[float]]:
-  data=json.dumps(json_data)
-  spilter=RecursiveCharacterTextSplitter(
-    separators=['\n\n', '\n', '\n\n\n', '.','\t'],
-    chunk_size=500,
-    chunk_overlap=0
-  )
-  chunks=spilter.split_text(data)
-  result_vectors = model.encode(chunks).tolist()
-  print(f"Generated {len(result_vectors)} embeddings.")
-  for id, value in enumerate(chunks):
-    upsert_vector=[]
-    vector_with_metadata = {"id":str(id), "vector": result_vectors[id], "metadata":{"client_id":client_id,"Data": value}}
-    upsert_vector.append(vector_with_metadata)
-    index.upsert(vectors=upsert_vector)
+
+def generate_embedding(json_data: str, client_id: str) -> None:
+    data = json.dumps(json_data)
+    spilter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '\n\n\n', '.', '\t'],
+        chunk_size=500,
+        chunk_overlap=0
+    )
+    chunks = spilter.split_text(data)
+    
+    for id, value in enumerate(chunks):
+        result_vectors = model.encode([value]).tolist()
+        vector_with_metadata = {"id": str(id), "vector": result_vectors[0], "metadata": {"client_id": client_id, "Data": value}}
+        index.upsert(vectors=[vector_with_metadata])
+
+    print(f"Generated {len(chunks)} embeddings.")
