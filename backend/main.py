be7e2afd5282b99bf988b6c738c02b104e005c19
sahil <sahilmulani501@gmail.com>
2024-03-02 16:02:30 +0530

    modal background task added

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from embed import start_embedding
-import asyncio
-
 app = FastAPI()
 
 class Name(BaseModel):
     name: str
 
-async def background_task(name):
-    await asyncio.sleep(0)  # Simulate some background work
-    await start_embedding(name=name)
 
 @app.get('/')
 async def home_page():
     return {
         "status": True,
-        "message": "backend working fine..."
+        "message":"backend working fine..."
     }
 
 @app.post("/generate_embeddings/")
 async def generate_embeddings(name: Name):
-    asyncio.create_task(background_task(name.name))
-    return {
-        "status": True,
-        "message": "Embedding process has started..."
-    }
+    try:
+        res = start_embedding(name=name.name)
+        return res
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
