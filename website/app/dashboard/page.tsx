4d81e5c7abb646802a83bf044e417e6b598221d6
sahil <sahilmulani501@gmail.com>
2024-03-23 19:34:17 +0530

    next starter code

--- website/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function Dashboard() {
-  return (
-    <div className="h-screen pt-10 flex justify-center">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
-}
