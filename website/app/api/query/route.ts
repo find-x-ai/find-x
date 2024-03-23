be1441ad8f491095bfc6be16df4e3f32f8837b87
sahil <sahilmulani501@gmail.com>
2024-03-23 19:46:12 +0530

    streaming test

---
+++ website/app/api/query/route.ts
@@ -0,0 +1,25 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+ 
+export async function POST(req: Request) {
+  const { query } = await req.json();
+
+  const response = await fetch("https://sahilm416--query.modal.run", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: query
+    })
+  }) as any
+  // const stream = OpenAIStream(response);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+  }
+
+
+  return new StreamingTextResponse(response.body)
+  
+}
