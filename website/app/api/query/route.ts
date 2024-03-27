49fdda2f862a5b96cdb4f9c09fe3b90966c5b79d
sahil <sahilmulani501@gmail.com>
2024-03-27 15:34:14 +0530

    test

--- website/app/api/query/route.ts
+++ website/app/api/query/route.ts
@@ -4,7 +4,7 @@ export const runtime = 'edge';
  
 export async function POST(req: Request) {
   const origin = req.headers.get('origin');
-
+  console.log("origin is " + origin);
   const { query } = await req.json();
 
   const response = await fetch("https://sahilm416--query.modal.run", {
@@ -20,6 +20,11 @@ export async function POST(req: Request) {
   if (!response.ok) {
     throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
   }
-  return new StreamingTextResponse(response.body)
+  return new StreamingTextResponse(response.body , {
+    headers: {
+      "Access-Control-Allow-Origin" : origin || "*",
+      "Content-Type" : "text/plain"
+    }
+  })
   
 }
