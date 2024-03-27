ceaba30545c2d54fe98568c30c21e1f57bc96962
sahil <sahilmulani501@gmail.com>
2024-03-27 15:41:00 +0530

    test

--- website/middleware.ts
+++ website/middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 
-export function middleware() {
+export function middleware(request : Request) {
     // retrieve the current response
     const res = NextResponse.next()
-
+    res.headers.set("Access-Control-Allow-Origin", "*");
     return res
 }
 
