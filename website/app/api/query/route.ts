9d3a7b1214c8f141cf44c78afbf1a6076bfc4c08
sahil <sahilmulani501@gmail.com>
2024-03-28 12:17:14 +0530

    refator headers

--- website/app/api/query/route.ts
+++ website/app/api/query/route.ts
@@ -1,26 +1,27 @@
 import { StreamingTextResponse } from "ai";
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "edge"
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
 export async function POST(req: NextRequest) {
   // Get the origin header from the request
   const origin = req.headers.get("Origin");
-
+  console.log("origin is " + origin);
   // Handle OPTIONS preflight requests
   if (req.method === "OPTIONS") {
-    return new NextResponse(null , {
+    return new NextResponse(null, {
       status: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Credentials": "true"
-      }
+        "Access-Control-Allow-Credentials": "true",
+      },
     });
   }
 
   const { query } = await req.json();
-  const apiResponse : any = await fetch("https://sahilm416--query.modal.run", {
+  const apiResponse: any = await fetch("https://sahilm416--query.modal.run", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -29,8 +30,17 @@ export async function POST(req: NextRequest) {
   });
 
   if (!apiResponse.ok) {
-    throw new Error(`Failed to fetch: ${apiResponse.status} ${apiResponse.statusText}`);
+    throw new Error(
+      `Failed to fetch: ${apiResponse.status} ${apiResponse.statusText}`
+    );
   }
 
-  return new StreamingTextResponse(apiResponse.body);
+  return new StreamingTextResponse(apiResponse.body, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
