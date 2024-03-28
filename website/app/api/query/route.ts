b271b05933538a4dc8f9e39eca8ca8aa9e303cca
sahil <sahilmulani501@gmail.com>
2024-03-28 11:07:59 +0530

    move headers to top

--- website/app/api/query/route.ts
+++ website/app/api/query/route.ts
@@ -8,6 +8,15 @@ export async function POST(req: NextRequest, res: NextResponse) {
   if (req.method === "OPTIONS") {
     return new NextResponse(null, { status: 200 }); // Return a 200 OK status for preflight requests
   }
+
+  res.headers.set("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.headers.set("Access-Control-Allow-Methods", "GET, DELETE, PATCH, POST, PUT");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+//   Optionally, you can allow credentials if needed
+  res.headers.set("Access-Control-Allow-Credentials", "true");
+
+
   const { query } = await req.json();
 
   const response = (await fetch("https://sahilm416--query.modal.run", {
@@ -25,12 +34,5 @@ export async function POST(req: NextRequest, res: NextResponse) {
       `Failed to fetch: ${response.status} ${response.statusText}`
     );
   }
-  return new StreamingTextResponse(response.body, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, DELETE, PATCH, POST, PUT",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Credentials": "true",
-    },
-  });
+  return new StreamingTextResponse(response.body);
 }
