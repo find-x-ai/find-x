2a1fb78923489c84510663ac1f9ac77d1466d24c
sahil <sahilmulani501@gmail.com>
2024-03-28 10:54:14 +0530

    test options check

--- website/middleware.ts
+++ website/middleware.ts
@@ -1,22 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware() {
-    // retrieve the current response
-    const res = NextResponse.next()
-    console.log("middleware ran")
-    // add the CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+export function middleware(req: NextRequest) {
+  // retrieve the current response
 
-    return res
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { status: 200, statusText: "success" });
+  }
+  const res = NextResponse.next();
+  console.log("middleware ran");
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
 }
 
 // specify the path regex to apply the middleware to
 export const config = {
-    matcher: '/api/:path*',
-}
+  matcher: "/api/:path*",
+};
