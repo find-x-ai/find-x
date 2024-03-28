05cec5bd46123b2097e1669b0a85a2b7d83fc917
sahil <sahilmulani501@gmail.com>
2024-03-28 12:47:49 +0530

    remove additional headers from response

--- website/app/api/query/route.ts
+++ website/app/api/query/route.ts
@@ -37,11 +37,6 @@ export async function POST(req: NextRequest) {
   }
 
   return new StreamingTextResponse(apiResponse.body, {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
+    status: 200
   });
 }
