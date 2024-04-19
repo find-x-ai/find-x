6a719d0f4217bfd034eac83622eb70535de4f4d6
sahil <sahilmulani501@gmail.com>
2024-04-19 20:43:41 +0530

    test puppeteer-core in serverless

--- website/app/api/fetch/route.ts
+++ website/app/api/fetch/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export const runtime = "edge";
+import edgeChromium from "chrome-aws-lambda";
+import  Puppeteer  from "puppeteer-core";
 
 export async function OPTIONS(request: NextRequest) {
   return new NextResponse(null, {
@@ -16,42 +16,28 @@ export async function OPTIONS(request: NextRequest) {
 
 export async function POST(req: NextRequest) {
   const { url } = await req.json();
+
+  const executablePath = await edgeChromium.executablePath;
   try {
-    const res = await fetch(url);
+     const browser = await Puppeteer.launch({
+      executablePath,
+      args: edgeChromium.args,
+      headless: true,
+     })
 
-    if (!res.ok) {
-      return new NextResponse(null,
-        {
-          status: 401,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          },
-        }
-      );
-    }
+     const page = await browser.newPage();
 
-    return new NextResponse(null,
+     const res = await page.goto(url);
+
+     const result = await res.text()
+
+    return NextResponse.json(
       {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-      }
+        data: result,
+      },
+      { status: 200 }
     );
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({
-      status: false,
-      message: "fetch failed",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
+
   }
 }
