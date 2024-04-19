c2ef5e95762a1cc31a0e16f133e9fdb36d65611f
sahil <sahilmulani501@gmail.com>
2024-04-19 22:04:42 +0530

    test

--- website/app/api/fetch/route.ts
+++ website/app/api/fetch/route.ts
@@ -1,10 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
-import chrome from '@sparticuz/chromium';
-import Puppeteer from 'puppeteer-core';
+import chrome from "@sparticuz/chromium";
+import Puppeteer from "puppeteer-core";
 
+const CHROMIUM_PATH =
+  "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
 
+async function getBrowser() {
+  if (process.env.VERCEL_ENV === "production") {
+    const chromium = await import("@sparticuz/chromium-min").then(
+      (mod) => mod.default
+    );
 
+    const puppeteerCore = await import("puppeteer-core").then(
+      (mod) => mod.default
+    );
 
+    const executablePath = await chromium.executablePath(CHROMIUM_PATH);
+
+    const browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath,
+      headless: chromium.headless,
+    });
+    return browser;
+  } else {
+    const puppeteer = await import("puppeteer").then((mod) => mod.default);
+
+    const browser = await puppeteer.launch();
+    return browser;
+  }
+}
 
 export async function OPTIONS(request: NextRequest) {
   return new NextResponse(null, {
@@ -21,18 +47,13 @@ export async function OPTIONS(request: NextRequest) {
 export async function POST(req: NextRequest) {
   const { url } = await req.json();
   try {
-    const browser = await Puppeteer.launch({
-      args: chrome.args,
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath(),
-      headless: 'new',
-      ignoreHTTPSErrors: true
-    });
-     const page = await browser.newPage();
+    const browser = await getBrowser();
 
-     const res = await page.goto(url);
+    const page = await browser.newPage();
 
-     const result = await res?.text()
+    const res = await page.goto(url);
+
+    const result = await res?.text();
 
     return NextResponse.json(
       {
@@ -41,10 +62,13 @@ export async function POST(req: NextRequest) {
       { status: 200 }
     );
   } catch (error) {
-     console.log(error)
+    console.log(error);
 
-     return NextResponse.json({
-      message:"failed to fetch data",
-     }, {status: 401})
+    return NextResponse.json(
+      {
+        message: "failed to fetch data",
+      },
+      { status: 401 }
+    );
   }
 }
