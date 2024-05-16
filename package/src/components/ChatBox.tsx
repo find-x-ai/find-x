3201bdae2c27ded2efdc2bd019d23f7e99115761
sahil <sahilmulani501@gmail.com>
2024-05-16 22:54:50 +0530

    initial setup for package

---
+++ package/src/components/ChatBox.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const ChatBox = () => {
+  return (
+    <div>
+      <h1>Hello from Find X</h1>
+    </div>
+  );
+};
+
+export default ChatBox;
---
+++ package/src/index.ts
@@ -0,0 +1,2 @@
+import "./style/global.css";
+export { default as ChatBox } from "./components/ChatBox"
