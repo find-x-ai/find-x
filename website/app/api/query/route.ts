ee10f0ef846624be3f193244df0d99edca76e487
sahil <sahilmulani501@gmail.com>
2024-07-03 19:08:02 +0530

    add : ai api

--- website/app/api/query/route.ts
+++ website/app/api/query/route.ts
@@ -1,9 +1,29 @@
-import { StreamingTextResponse } from "ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextRequest, NextResponse } from "next/server";
+import { StreamingTextResponse, GoogleGenerativeAIStream } from "ai";
 
 export const runtime = "edge";
 
-export async function OPTIONS(request: NextRequest){
+const instructions = `You're an ai search assistant, who helps users to solve their queries. You get the following XML as input :
+<message>
+   <query>User's query about the website</query>
+   <chunks>
+      <page>
+        <url>https://example.com/abc</url>
+        <content>Data found on the page</content>
+      </page>
+      <page>
+        <url>https://example.com/xyz</url>
+        <content>Data found on the second page</content>
+      </page>        
+   </chunks>
+</message>
+Response :
+You just take the XML data and check wheather is there any chance that the query in XML data is related to the page data , if no then deny the request by "Cannot assists with this request." or "Please provide more context !" according yo the situation. If data and query
+ are related then undersatnd the query and prepare a answer for that query from the recieved page data and respond with it. You can greet if query is a greeting like "How can i assist you today ?". Keep the responses short , you are allowed to summarise the pages data . After response you add the "<-$#$->" symbol and add page urls separated by comma. No need to add any symbol and urls if query was a greeting or denial of request.
+`;
+
+export async function OPTIONS(request: NextRequest) {
   return new NextResponse(null, {
     status: 200,
     headers: {
@@ -17,26 +37,28 @@ export async function OPTIONS(request: NextRequest){
 
 export async function POST(req: NextRequest) {
   // Get the origin header from the request
-  const origin = req.headers.get("origin");
-  console.log("origin is " + origin);
-
-
   const { query } = await req.json();
-  const apiResponse: any = await fetch("https://sahilm416--query.modal.run", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ client: "100", query: query }),
+
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 19000,
+    responseMimeType: "text/plain",
+  };
+
+  const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY!);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: instructions,
+    generationConfig: generationConfig,
   });
 
-  if (!apiResponse.ok) {
-    throw new Error(
-      `Failed to fetch: ${apiResponse.status} ${apiResponse.statusText}`
-    );
-  }
+  const res = await model.generateContentStream(query);
 
-  return new StreamingTextResponse(apiResponse.body, {
+  const stream = GoogleGenerativeAIStream(res);
+
+  return new StreamingTextResponse(stream, {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
