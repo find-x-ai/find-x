f5de4d82e0352c4dd596ce5aad83f427540f891c
sahil <sahilmulani501@gmail.com>
2024-07-17 21:37:20 +0530

    refactor: minor ui changes

--- website/components/home/video.tsx
+++ website/components/home/video.tsx
@@ -10,7 +10,7 @@ export const Video = () => {
       <div className="w-full">
         <div className="relative w-full pt-[56.25%] overflow-hidden rounded-lg">
           <iframe
-            className="absolute top-0 left-0 w-full h-full rounded-lg scale-125"
+            className="absolute top-0 left-0 w-full h-full rounded-lg"
             src="https://www.youtube.com/embed/dNVZFkMcehg?autoplay=1&loop=1&controls=0&playlist=dNVZFkMcehg&mute=1"
             title="YouTube video"
             allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
