101ad04e450644c1e647577ad82ad5dd509d1c03
Sohel Mujawar <sohelmujawar1807@gmail.com>
2024-07-17 17:00:43 +0530

    some minor changes

--- modal/upsert.py
+++ modal/upsert.py
@@ -22,48 +22,54 @@ class SentenceTransformerEmbeddings(Embeddings):
     def embed_query(self, text: str) -> list[float]:
         return self.model.encode([text])[0]
 
-@stub.function(secrets=[modal.Secret.from_name("upstash-token"), modal.Secret.from_name("upstash-url")], timeout=1000)
+@stub.function(secrets=[modal.Secret.from_name("my-custom-secret"),modal.Secret.from_name("upstash-token"), modal.Secret.from_name("upstash-url")] ,timeout=1000)
 @web_endpoint(label="embed", method="POST")
 def generate_embedding(requestData: Dict):
     from langchain_experimental.text_splitter import SemanticChunker
     from upstash_vector import Index
+    key = os.environ["key_secret"]
     
     data = requestData["data"]  # data is array of dict i.e. [{"url": string, "content": string}]
     client_id = requestData["client"]  # client id is string
+    secret_key=requestData["secret_key"]
+    
+    
+    if(secret_key!=key):
+        return {"error": "Invalid upsert key!"}
+    else:
+        my_list = []
+        for item in data:
+            url = item.get("url", "")
+            content = item.get("content", "")
+            my_list.append({'content': content, 'url': url})
 
-    my_list = []
-    for item in data:
-        url = item.get("url", "")
-        content = item.get("content", "")
-        my_list.append({'content': content, 'url': url})
+        upstash_token = os.environ["Token"]
+        upstash_url = os.environ["URL"]
 
-    upstash_token = os.environ["Token"]
-    upstash_url = os.environ["URL"]
+        index = Index(url=upstash_url, token=upstash_token)  # initialize the vector index
+        chunks=[]
+        text_splitter = SemanticChunker(SentenceTransformerEmbeddings(),breakpoint_threshold_type="standard_deviation")
+        try:
+            for id, value in enumerate(my_list):
+                content = value['content']
+                url = value['url']
+                docs = text_splitter.create_documents([content])
 
-    index = Index(url=upstash_url, token=upstash_token)  # initialize the vector index
-    chunks=[]
-    text_splitter = SemanticChunker(SentenceTransformerEmbeddings(),breakpoint_threshold_type="standard_deviation")
-    try:
-        for id, value in enumerate(my_list):
-            content = value['content']
-            url = value['url']
-            docs = text_splitter.create_documents([content])
-
-            for chunk_id, doc in enumerate(docs):
-                index.upsert(
-                    vectors=[
-                        (f"{url}_{client_id}", doc.page_content, {"client_id": client_id, "url": url, "Data": doc.page_content}),
-                    ],
-                    namespace=client_id
-                )
-                chunks.append(doc.page_content)
-        return {
-            "Status": "success",
-            "message": "successfully generated embedding...",
-            "chunks":chunks,
-        }
-    except Exception as e:
+                for chunk_id, doc in enumerate(docs):
+                    index.upsert(
+                        vectors=[
+                            (f"{url}_{client_id}", doc.page_content, {"client_id": client_id, "url": url, "Data": doc.page_content}),
+                        ],
+                        namespace=client_id
+                    )
+                    chunks.append(doc.page_content)
             return {
-                "Status": "error",
-                "message": str(e),
+                "Status": "success",
+                "message": "successfully generated embedding...",
+                "chunks":chunks,
             }
+        except Exception as e:
+                return {
+                    "Status": "error",
+                    "message": str(e),
+                }
