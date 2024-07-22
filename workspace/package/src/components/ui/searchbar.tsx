7c08e21e171c77f2f1054046463113e70329a860
sahil <sahilmulani501@gmail.com>
2024-07-22 18:44:50 +0530

    fix: bg color of searchbar and upgrade

--- workspace/package/src/components/ui/searchbar.tsx
+++ workspace/package/src/components/ui/searchbar.tsx
@@ -10,7 +10,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({
   handleSubmit,
   setIsOpen,
 }) => (
-  <div className="f-flex f-w-full f-h-14 f-bg-[#fcfdf8] f-rounded-md f-overflow-hidden f-z-10 f-border f-border-zinc-800/90">
+  <div className="f-flex f-w-full f-h-14 f-bg-zinc-100 f-rounded-md f-overflow-hidden f-z-10 f-border f-border-zinc-800/90">
     <div className="f-flex f-justify-center f-items-center f-py-2 f-px-3">
       <SearchIcon />
     </div>
