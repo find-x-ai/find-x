b9531d07493ff35392723361a0e1818193a63956
sahil <sahilmulani501@gmail.com>
2024-07-27 00:39:18 +0530

    refactor: reduce the page wait

--- modal/scrape.py
+++ modal/scrape.py
@@ -34,7 +34,7 @@ async def get_links(request: Dict):
             try:
                 page = await browser.new_page()
                 await page.goto(cur_url, timeout=30000)
-                await page.wait_for_timeout(5000) 
+                await page.wait_for_timeout(2000) 
                 # Extracting absolute links only and avoiding duplicates
                 links = await page.eval_on_selector_all("a[href]", """
                     (baseUrl) => {
