1072063ee2bf46588801730dc22df74335fc2a3e
sahil <sahilmulani501@gmail.com>
2024-07-28 22:59:05 +0530

    add: rust commands for persistant storage

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -2,9 +2,29 @@
 use reqwest::blocking::get;
 
 
+mod local_storage {
+    use std::collections::HashMap;
+    use std::sync::Mutex;
+
+    lazy_static::lazy_static! {
+        static ref STORAGE: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
+    }
+
+    pub fn set(key: String, value: String) {
+        let mut storage = STORAGE.lock().unwrap();
+        storage.insert(key, value);
+    }
+
+    pub fn get(key: String) -> Option<String> {
+        let storage = STORAGE.lock().unwrap();
+        storage.get(&key).cloned()
+    }
+}
+
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![check_url])
+        .invoke_handler(tauri::generate_handler![check_url,set_item,get_item])
         .run(tauri::generate_context!())
         .expect("failed to run app");
 }
@@ -21,4 +41,14 @@ fn check_url(url: &str) -> bool {
       }
       Err(_) => false,
   }
+}
+
+#[tauri::command]
+fn set_item(key: String, value: String) {
+    local_storage::set(key, value);
+}
+
+#[tauri::command]
+fn get_item(key: String) -> Option<String> {
+    local_storage::get(key)
 }
