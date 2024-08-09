319b8c565d2ab4ef00dd7db3431cc80218ac331e
sahil <sahilmulani501@gmail.com>
2024-08-09 12:52:36 +0530

    refactor: team details and icons

--- website/components/team/members.tsx
+++ website/components/team/members.tsx
@@ -2,13 +2,15 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Link as LinkIcon } from "lucide-react";
 
 interface Member {
   name: string;
   email: string;
   profile: string;
   role: string;
+  about: string;
+  x: string;
 }
 
 const membersList: Member[] = [
@@ -17,18 +19,30 @@ const membersList: Member[] = [
     email: "sahil.findx@gmail.com",
     profile: "https://github.com/sahilm416.png",
     role: "Founder and CEO",
+    about: `Sahil Mulani is the CEO and founder of Find-X, an AI-powered search engine.\n
+    With a background in data science and software engineering, Sahil is passionate about making online information easier to access and more relevant.\n
+    He leads the team at Find-X with a focus on innovation and efficiency, ensuring the search engine delivers accurate results quickly.`,
+    x: "https://x.com/sahil__501",
   },
   {
     name: "Sohel Mujawar",
     email: "sohelmujawar1807@gmail.com",
     profile: "https://github.com/sohel1807.png",
-    role: "Founder and Data engg.",
+    role: "Founder and Data Engineer",
+    about: `Sohel Mujawar is the founder and Data Engineer at Find-X.\n
+    He has extensive practical experience in data science and machine learning, which he leverages to optimize data processing and analysis within the platform.\n
+    His expertise in building scalable data systems has been instrumental in the development of the search engine's core functionalities.`,
+    x: "https://x.com/sohel_mujawar07",
   },
   {
     name: "Saad Momin",
-    email: "saadmomin@gmail.com",
+    email: "saadmomin5555@gmail.com",
     profile: "https://github.com/saadmomin2903.png",
-    role: "Founder and Data engg.",
+    role: "Founder and Data Engineer",
+    about: `Saad Momin is a founder and Data Engineer at Find-X.\n
+    He works closely with Sohel Mujawar, providing crucial support in developing and maintaining the platform's data infrastructure.\n
+    As a helping hand to the team, Saad ensures that all systems run smoothly and efficiently, playing a key role in troubleshooting and optimizing processes.\n`,
+    x: "https://x.com/saadlegend0",
   },
 ];
 
@@ -106,7 +120,7 @@ export const Members: React.FC = () => {
       <AnimatePresence mode="wait">
         <motion.div
           key={selectedMember ? selectedMember.name : "default"}
-          className={`w-full md:w-2/3 h-[calc(100vh-70px)] p-5 justify-center ${
+          className={`w-full md:w-2/3 p-5 justify-center ${
             showMemberList ? "hidden md:flex" : "flex"
           }`}
           initial={{ opacity: 0, filter: "blur(10px)" }}
@@ -116,7 +130,7 @@ export const Members: React.FC = () => {
         >
           {selectedMember ? (
             <motion.div
-              className="w-full max-w-2xl flex flex-col items-center"
+              className="w-full flex flex-col"
               initial={{ y: 20, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 0.1 }}
@@ -127,31 +141,69 @@ export const Members: React.FC = () => {
               >
                 <ChevronLeft /> back
               </button>
-              <img
-                src={selectedMember.profile}
-                alt={selectedMember.name}
-                className="w-32 h-32 md:w-64 md:h-64 rounded-full object-cover border-4 border-zinc-300 shadow-lg mb-4 md:mb-8"
-              />
-              <h1 className="text-2xl md:text-4xl font-bold mb-2 text-center">
-                {selectedMember.name}
-              </h1>
-              <h2 className="text-xl md:text-2xl text-zinc-600 mb-2 md:mb-4 text-center">
-                {selectedMember.role}
-              </h2>
-              <p className="text-lg md:text-xl mb-4 md:mb-8 text-center">
-                {selectedMember.email}
-              </p>
+              <div className="flex flex-col py-10 md:pt-0">
+                <div className="flex gap-3">
+                  <img
+                    src={selectedMember.profile}
+                    alt={selectedMember.name}
+                    className="w-16 h-16 md:w-20 md:h-20 rounded-full object-cover border-4 border-zinc-300 shadow-lg"
+                  />
+                  <div className="flex flex-col items-start justify-center ">
+                    <h1 className="text-xl md:text-2xl font-semibold text-center">
+                      {selectedMember.name}
+                    </h1>
+                    <h2 className="text-lg md:text-xl text-zinc-600 text-center">
+                      {selectedMember.role}
+                    </h2>
+                  </div>
+                </div>
+              </div>
               <div className="w-full rounded-lg border border-zinc-800 bg-zinc-200 p-4 md:p-6">
                 <h3 className="text-lg md:text-xl font-semibold mb-2 md:mb-4">
-                  About {selectedMember.name}
+                  About
                 </h3>
-                <p className="text-sm md:text-base text-zinc-700">
-                  {selectedMember.name} is a key member of our team, bringing
-                  valuable expertise and leadership to their role as{" "}
-                  {selectedMember.role}. Their contributions have been
-                  instrumental in shaping the direction and success of our
-                  projects.
+                <p className="text-sm md:text-base text-zinc-700 whitespace-pre-line">
+                  {selectedMember.about}
                 </p>
+              </div>
+              <div className="px-4 py-6 md:px-6 md:py-8 flex flex-col gap-5">
+                <div className="text-xl font-semibold">
+                  Reach out to {selectedMember.name}
+                </div>
+                <div>
+                  <ul className=" list-inside list-disc space-y-3 pb-5">
+                    <li className="hover:underline flex gap-2 items-center w-full">
+                      <LinkIcon className="w-[17px] h-[17px] text-neutral-500" />
+                      <img
+                        className="w-[25px] "
+                        src="/github.png"
+                        alt="github icon"
+                      />
+                      <a
+                        target="_blanc"
+                        href={selectedMember.profile.split(".png")[0]}
+                      >
+                        Github
+                      </a>
+                    </li>
+                    <li className="hover:underline flex gap-2 items-center w-full">
+                      <LinkIcon className="w-[17px] h-[17px] text-neutral-500" />
+                      <img className="w-[25px] " src="/x.png" alt="x icon" />
+                      <a target="_blanc" href={selectedMember.x}>
+                        X (twitter)
+                      </a>
+                    </li>
+                    <li className="hover:underline flex gap-2 items-center w-full">
+                      <LinkIcon className="w-[17px] h-[17px] text-neutral-500" />
+                      <img
+                        className="w-[25px] "
+                        src="/email.png"
+                        alt="email icon"
+                      />
+                      <a href={`mailto:${selectedMember.email}`}>Email</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </motion.div>
           ) : (
