e12ee02ab7a24ab784f25754c56834e9537fa30f
sahil <sahilmulani501@gmail.com>
2024-08-24 22:13:27 +0530

    fix: minor docs isuues

--- website/components/docs/custom-demo.tsx
+++ website/components/docs/custom-demo.tsx
@@ -3,7 +3,7 @@ import { toggleChatBox } from "find-x-ai";
 
 export function CustomChatComponent(){
   return (
-    <button className="py-2 px-4 bg-zinc-800 text-white rounded-md my-2" onClick={ ()=> toggleChatBox() }>
+    <button className="py-2 px-4 bg-green-500 text-white rounded-md my-2" onClick={ ()=> toggleChatBox() }>
        Search here...
     </button>
   )
