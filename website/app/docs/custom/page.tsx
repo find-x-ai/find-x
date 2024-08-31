4088fd723d5b1c3658d80ecc23d7509b5c2799fd
sahil <sahilmulani501@gmail.com>
2024-08-31 13:09:45 +0530

    add: metadata for custom component docs

--- website/app/docs/custom/page.tsx
+++ website/app/docs/custom/page.tsx
@@ -1,4 +1,10 @@
 import CustomDocs from "@/components/docs/custom.mdx"
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Custom Component",
+};
+
 const page = () => {
   return (
     <div>
