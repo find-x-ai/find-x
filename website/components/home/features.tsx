ca3e6ca0c832af688363fa1edbdd3157b40fa173
sahil <sahilmulani501@gmail.com>
2024-09-02 23:46:20 +0530

    fix: minor css

--- website/components/home/features.tsx
+++ website/components/home/features.tsx
@@ -1,14 +1,14 @@
 import { ChevronsUp, Route, TextSearch } from "lucide-react";
 export const Features = () => {
   return (
-    <div className="w-full flex flex-col gap-10 items-center text-center">
+    <div className="w-full flex flex-col gap-10 items-center text-center py-10">
       <div className=" space-y-3">
         <h3 className="text-xl">Introduction</h3>
         <h2 className="text-[#20A0B5] text-4xl md:text-5xl">What is it?</h2>
       </div>
       <div className="w-full flex flex-col gap-3">
         <div className="w-full flex flex-col md:flex-row gap-3">
-          <div className="w-full bg-[#F6F7F9] rounded-lg md:rounded-[20px_0px_0px_0px] p-10 flex items-end justify-center text-center">
+          <div className="w-full bg-[#F6F7F9] rounded-lg md:rounded-[20px_0px_0px_0px] p-12 flex items-center justify-center text-center">
             <div className="space-y-2">
               <h3 className="font-[600] text-lg">Advanced Search</h3>
               <p className="text-zinc-600">
@@ -16,7 +16,7 @@ export const Features = () => {
               </p>
             </div>
           </div>
-          <div className="w-full bg-[#F6F7F9] rounded-lg md:rounded-[0px_20px_0px_0px] p-10 flex items-end justify-center text-center">
+          <div className="w-full bg-[#F6F7F9] rounded-lg md:rounded-[0px_20px_0px_0px] p-12 flex items-center justify-center text-center">
             <div className="space-y-2">
               <h3 className="font-[600] text-lg">Not Just Text</h3>
               <p className="text-zinc-600">
@@ -27,19 +27,19 @@ export const Features = () => {
           </div>
         </div>
         <div className="flex flex-col md:flex-row gap-3">
-          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-[0px_0px_0px_20px] flex justify-between items-center">
+          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-[0px_0px_0px_20px] flex gap-5 justify-center items-center">
             <h2 className="text-lg tracking-wider font-medium">
               Faster response time
             </h2>{" "}
             <ChevronsUp className="w-[30px] h-[30px] text-zinc-500" />
           </div>
-          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-none flex justify-between items-center">
+          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-none flex gap-5 justify-center items-center">
             <h2 className="text-lg tracking-wider font-medium">
               Efficient search
             </h2>{" "}
             <TextSearch className="w-[30px] h-[30px] text-zinc-500" />
           </div>
-          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-[0px_0px_20px_0px] flex justify-between items-center">
+          <div className="w-full text-center p-10 cursor-default bg-[#F6F7F9] rounded-lg md:rounded-[0px_0px_20px_0px] flex gap-5 justify-center items-center">
             <h2 className="text-lg tracking-wider font-medium">
               Easy integration
             </h2>{" "}
