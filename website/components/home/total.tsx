7cec8e78bf9ffcb772c7450b36774a3c47a83651
sahil <sahilmulani501@gmail.com>
2024-09-02 10:28:44 +0530

    fix: font in total requests

--- website/components/home/total.tsx
+++ website/components/home/total.tsx
@@ -2,7 +2,7 @@ export const TotalRequests = async ({ count }: { count: number }) => {
   return (
     <div className="flex items-center justify-center p-6 md:p-10 rounded-xl">
       <div className="w-full max-w-[600px] flex flex-col items-center md:space-y-0 md:flex-row gap-3 md:justify-center tracking-widest">
-        <h1 className="text-2xl md:text-3xl font-semibold text-zinc-700 tracking-wide">
+        <h1 className="text-2xl md:text-3xl font-medium text-zinc-700 tracking-wide">
           Served total
         </h1>
         <div className="flex flex-col md:flex-row md:gap-2 gap-3 items-center space-x-2">
