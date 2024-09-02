cae728b7d0dc2cb2c7f141047bd91267ad2c01dc
sahil <sahilmulani501@gmail.com>
2024-09-02 08:31:32 +0530

    refactor: padding

--- website/components/pricing/sales.tsx
+++ website/components/pricing/sales.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Sales = () => {
   return (
-    <div className="w-full p-6 md:p-10 my-5 flex justify-center bg-[#F6F7F9] border border-zinc-800 rounded-lg">
+    <div className="w-full p-5  md:p-10 my-5 flex justify-center bg-[#F6F7F9] border border-zinc-800 rounded-lg">
       <div className="w-full max-w-[500px] flex justify-center gap-5 items-center">
         <div className="">
           <h2 className="text-xl md:text-2xl font-semibold">
@@ -11,10 +11,10 @@ export const Sales = () => {
         </div>
         <div>
           <Link
-            className="w-[120px] px-5 py-4 rounded-md bg-zinc-800 text-white"
+            className="w-[120px] md:px-5 px-5 py-4 rounded-md bg-zinc-800 text-white"
             href={"/request"}
           >
-            Request Access
+            Request
           </Link>
         </div>
       </div>
