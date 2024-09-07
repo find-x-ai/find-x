2db1ff8de55ff8403daca8a24083ac11a85aef93
sahil <sahilmulani501@gmail.com>
2024-09-07 23:32:36 +0530

    refactor: UI/UX

--- website/app/pricing/page.tsx
+++ website/app/pricing/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
   return (
     <div className="w-full min-h-[calc(100vh-70px)] px-5">
       <div>
-        <h1 className="text-3xl text-zinc-700 py-10 font-semibold">Worry about your app, not bills!</h1>
+        <h1 className="text-3xl text-[#20A0B5] py-10 font-semibold">Worry about your app, not bills!</h1>
       </div>
       <Cards />
       <Sales/>
