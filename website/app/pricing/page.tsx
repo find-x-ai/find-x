9df98b470a0a682292925b7744ea2c74b53a28f9
sahil <sahilmulani501@gmail.com>
2024-09-07 23:33:23 +0530

    refactor: prettier formatting

--- website/app/pricing/page.tsx
+++ website/app/pricing/page.tsx
@@ -1,4 +1,4 @@
-import { Cards , Sales } from "@/components/pricing";
+import { Cards, Sales } from "@/components/pricing";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -10,10 +10,12 @@ const page = () => {
   return (
     <div className="w-full min-h-[calc(100vh-70px)] px-5">
       <div>
-        <h1 className="text-3xl text-[#20A0B5] py-10 font-semibold">Worry about your app, not bills!</h1>
+        <h1 className="text-3xl text-[#20A0B5] py-10 font-semibold">
+          Worry about your app, not bills!
+        </h1>
       </div>
       <Cards />
-      <Sales/>
+      <Sales />
     </div>
   );
 };
