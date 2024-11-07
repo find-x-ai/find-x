17d2bb7670394cbc1badca6d9c8260f4dd709922
sahil <sahilmulani501@gmail.com>
2024-11-07 17:32:58 +0530

    Reverting to an earlier state

---
+++ website/app/dashboard/new/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+
+const NewIndexPage = () => {
+  return (
+    <div className="container mx-auto p-6">
+        
+    </div>
+  )
+}
+
+export default NewIndexPage
