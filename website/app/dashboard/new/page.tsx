24c15e8294eae61c6800bc938a215db04ef3b33d
sahil <sahilmulani501@gmail.com>
2024-11-07 19:10:04 +0530

    new hero section

--- website/app/dashboard/new/page.tsx
+++ website/app/dashboard/new/page.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
 
 const NewIndexPage = () => {
-  return (
-    <div className="container mx-auto p-6">
-        
-    </div>
-  )
-}
+  return <div className="container mx-auto p-6"></div>;
+};
 
-export default NewIndexPage
+export default NewIndexPage;
--- website/components/home/hero.tsx
+++ website/components/home/hero.tsx
@@ -1,61 +1,39 @@
-"use client";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { MoveRight, PhoneCall } from 'lucide-react';
+import Link from 'next/link';
 
-import { motion } from "framer-motion";
-import { ChevronRight } from "lucide-react";
-
-export const Hero = ({ version }: { version: string }) => {
-  return (
-    <motion.div
-      initial={{ y: -10, filter: "blur(5px)", opacity: 0 }}
-      whileInView={{ y: 0, filter: "blur(0px)", opacity: 1 }}
-      viewport={{ once: true }}
-      className="w-full max-w-[1200px] text-center flex flex-col gap-5 mx-auto py-10"
-    >
-      <div className="flex flex-col items-center gap-5">
-        <Link target="_blank" href={'https://www.npmjs.com/package/find-x-ai'} className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-          <span className="absolute inset-0 overflow-hidden rounded-full">
-            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-          </span>
-          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-            <span>Just released {version}</span>
-            <svg
-              fill="none"
-              height="16"
-              viewBox="0 0 24 24"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10.75 8.75L14.25 12L10.75 15.25"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-              />
-            </svg>
-          </div>
-          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-        </Link>
-        <h1 className=" text-5xl sm:text-6xl  text-[#f7f8f8] tracking-tighter leading-[1.1]">
-          The most comprehensive <br />
-          <span className="gradient-text"> AI search</span> for web
-        </h1>
-        <p className="text-[#b3b4b4] text-lg">
-          Upgrade your default search and search better with Find-X.
-        </p>
+export const Hero = ({ version }: { version: string }) => (
+  <div className="w-full">
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center justify-center gap-8 py-16 lg:py-28">
+        <div>
+          <Button variant="secondary" size="sm" className="gap-4" asChild>
+            <Link href={`https://npmjs.org/find-x-ai`}>
+              Just released {version} <MoveRight className="h-4 w-4" />
+            </Link>
+          </Button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="max-w-2xl text-white text-center font-regular text-5xl tracking-tighter md:text-7xl">
+            The future of internal search for websites
+          </h1>
+          <p className="max-w-2xl text-center text-lg text-muted-foreground leading-relaxed tracking-tight md:text-xl">
+            Searching for right thing with old keyword based searches is a thing of the past. Transform your website into data and search with the power of AI.
+          </p>
+        </div>
+        <div className="flex flex-row gap-3">
+          <Button size="lg" className="gap-4" variant="outline" asChild>
+            <Link href="/dashboard">
+              Get started <MoveRight className="h-4 w-4" />
+            </Link>
+          </Button>
+          <Button size="lg" className="gap-4" asChild>
+            <Link href={'/docs'}>
+              Documentation 
+            </Link>
+          </Button>
+        </div>
       </div>
-      <div className="flex sm:flex-row  gap-5 items-center justify-center">
-        <Link href={'/dashboard'} className="py-2 px-5 w-full sm:w-auto bg-white shadow-[0px_0px_200px_#059669]-  rounded-md text-black">
-          Get started
-        </Link>
-        <Link
-          className="border sm:w-auto w-full border-[#232424] py-2 px-5 rounded-md text-white flex items-center justify-center group"
-          href={"/docs/getting_started"}
-        >
-          Documentation
-        </Link>
-      </div>
-    </motion.div>
-  );
-};
+    </div>
+  </div>
+);
