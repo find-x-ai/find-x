3bc6fb5a2fe68127b3abb913304725812cf097b8
sahil <sahilmulani501@gmail.com>
2025-01-18 09:48:27 +0530

    fix large content fetch

--- website/app/dashboard/indexing/page.tsx
+++ website/app/dashboard/indexing/page.tsx
@@ -3,10 +3,14 @@ import { AllIndexes } from "./_components";
 import { getIndexes } from "@/actions/indexing";
 import { Index } from "@/actions/types";
 
+type IndexWithContentLength = Omit<Index, "content"> & {
+  content_length: number;
+};
+
 export const revalidate = 0;
 const page = async () => {
-  const indexes = (await getIndexes()) as Index[];
-  return <AllIndexes indexes={indexes} />;
+  const indexes = (await getIndexes()) as IndexWithContentLength[];
+  return <AllIndexes />;
 };
 
 export default page;
