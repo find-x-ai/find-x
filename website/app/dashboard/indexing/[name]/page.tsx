b89e684ef258710151a2457369ac8911aae1f9f9
sahil <114013781+Sahilm416@users.noreply.github.com>
2026-05-25 23:07:29 +0530

    Refactor indexing page to use IndexScreen component

--- website/app/dashboard/indexing/[name]/page.tsx
+++ website/app/dashboard/indexing/[name]/page.tsx
@@ -1,49 +1,22 @@
-import { getSession } from "@/actions/auth";
-import { sql } from "@/lib/db";
-import { redirect } from "next/navigation";
-import { Index } from "@/actions/types";
-import { Screen } from "../_components";
-import { redis } from "@/lib/db";
+import { getSession } from "@/actions/auth"
+import { redirect } from "next/navigation"
+import { IndexScreen } from "./_components/index-screen"
 
-export const revalidate = 0;
+export const revalidate = 0
 
 const page = async ({ params }: { params: { name: string } }) => {
-  const session = await getSession();
+  const session = await getSession()
   if (!session || !session.data) {
-    redirect("/login");
+    redirect("/login")
   }
 
-  const name = decodeURIComponent(params.name);
+  const name = decodeURIComponent(params.name)
 
-  const indexes =
-    (await sql`SELECT * FROM indexes WHERE name = ${name} and user_id = ${session.data.id}`) as Index[];
-  if (
-    !indexes ||
-    indexes.length === 0 ||
-    indexes[0].user_id !== session.data.id
-  ) {
-    redirect("/dashboard/indexing");
-  }
-  let processData;
-  if(indexes[0].status === 'deploying') {
-     processData = await redis.get(`process_${indexes[0].id}`) as {
-      queueLength: number;
-      scrapedDataLength: number;
-      visitedLength: number;
-      percentage: number;
-    };
-  }
-  
   return (
     <main className="flex flex-col w-full h-full">
-      <Screen index={indexes[0]} processData={processData || {
-        queueLength: 0,
-        scrapedDataLength: 0,
-        visitedLength: 0,
-        percentage: 0,
-      }} />
+      <IndexScreen name={name} />
     </main>
-  );
-};
+  )
+}
 
-export default page;
+export default page
